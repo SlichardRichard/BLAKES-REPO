51baa1bc215767b32d775e7fd9656dce41d165b0
MrClevelandBrown <blakeo0208@gmail.com>
2024-11-03 21:01:34 -0800

    fix

--- Unity Intro PM/Assets/Scripts/ShotScript.cs
+++ Unity Intro PM/Assets/Scripts/ShotScript.cs
@@ -8,7 +8,7 @@ public class ShotScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-       if(other.CompareTag("Wall") || (other.CompareTag("Ground")) || (other.CompareTag("basic enemy")) || (other.CompareTag("KeyDoor")))
+       if(other.CompareTag("LevelGeo") || (other.CompareTag("basic enemy")) || (other.CompareTag("KeyDoor")))
             Destroy(gameObject);
     }
 }
